fdc668a43a8472a3b92db93b9cc76e31e7586676
Shannon-Kioko <shannonkaluki@gmail.com>
2024-02-07 05:19:16 +0300

    Standard convention

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module renders places, states ... using Flask
+"""
+from flask import Flask, render_template, Markup
+from models import storage
+import sys
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """
+    reload the storage after each request
+    """
+    storage.close()
+
+
+@app.route("/hbnb", strict_slashes=False)
+def states_cities_list():
+    """
+    states and cities are sorted by name
+    and amenities
+    """
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    for state in states:
+        state.cities.sort(key=lambda x: x.name)
+    amenities = list(storage.all("Amenity").values())
+    amenities.sort(key=lambda x: x.name)
+    places = list(storage.all("Place").values())
+    places.sort(key=lambda x: x.name)
+    for place in places:
+        place.description = Markup(place.description)
+    return render_template(
+        '100-hbnb.html',
+        states=states,
+        amenities=amenities,
+        places=places
+    )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
